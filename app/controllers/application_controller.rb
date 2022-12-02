f326f3a7ac8f77c8ef013752434d1d84b39eb464
madelinedulchin <madeline.dulchin@gmail.com>
2022-12-02 11:20:05 -0500

    commit tree 13d4897c97b5ec5a562605e81eeed5c160ea81a6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  # before_action :authorize
 
+  # private
+
+  # def authorize
+  #   render json: {error: ""}
+  # end
 end
